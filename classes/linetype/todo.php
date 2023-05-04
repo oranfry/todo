3a6fef0c2971161bd6ce1ed4ff646d911a7afbb5
Oran Fry <oran@emerg.co>
2023-05-04 22:37:02 +1200

    Fields
    Add dependency on simple_fields. Conform to new signature for validate, and make sure to call parent.

--- classes/linetype/todo.php
+++ classes/linetype/todo.php
@@ -2,20 +2,24 @@
 
 namespace todo\linetype;
 
+use simplefields\traits\SimpleFields;
+
 class todo extends \jars\Linetype
 {
+    use SimpleFields;
+
     function __construct()
     {
         $this->table = 'todo';
 
-        $this->simple_strings('description');
+        $this->simple_string('description');
 
         $this->fields['created'] = fn ($records) : string => $records['/']->created;
     }
 
-    function validate($line)
+    function validate($line): array
     {
-        $errors = [];
+        $errors = parent::validate($line);
 
         if ($line->description == null) {
             $errors[] = 'no description';
